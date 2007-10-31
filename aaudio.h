f626ab9ebb680935612ce80f88a676c04007a1ba
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2007-10-31 14:12:37 +0000

    Added aaudio.c/.h to handle low level ALSA stuff.
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@2 b9971538-fbc6-430a-b57f-16e266280561

---
+++ aaudio.h
@@ -0,0 +1,28 @@
+
+static int set_hwparams(snd_pcm_t *handle,
+			snd_pcm_hw_params_t *params,
+			snd_pcm_access_t access);
+static int set_swparams(snd_pcm_t *handle, snd_pcm_sw_params_t *swparams);
+static int xrun_recovery(snd_pcm_t *handle, int err);
+static int write_loop(snd_pcm_t *handle,
+		      signed short *samples,
+		      snd_pcm_channel_area_t *areas);
+static int wait_for_poll(snd_pcm_t *handle, struct pollfd *ufds, unsigned int count);
+static int write_and_poll_loop(snd_pcm_t *handle,
+			       signed short *samples,
+			       snd_pcm_channel_area_t *areas);
+static void async_callback(snd_async_handler_t *ahandler);
+static int async_loop(snd_pcm_t *handle,
+		      signed short *samples,
+		      snd_pcm_channel_area_t *areas);
+static void async_direct_callback(snd_async_handler_t *ahandler);
+static int async_direct_loop(snd_pcm_t *handle,
+			     signed short *samples ATTRIBUTE_UNUSED,
+			     snd_pcm_channel_area_t *areas ATTRIBUTE_UNUSED);
+static int direct_loop(snd_pcm_t *handle,
+		       signed short *samples ATTRIBUTE_UNUSED,
+		       snd_pcm_channel_area_t *areas ATTRIBUTE_UNUSED);
+static int direct_write_loop(snd_pcm_t *handle,
+			     signed short *samples,
+			     snd_pcm_channel_area_t *areas);
+
