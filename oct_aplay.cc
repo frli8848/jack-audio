9655a2563e0ff333d698f1a8766ec8b6112f89fd
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2007-11-05 17:07:25 +0000

    Added more code for interrupt driven mmap support. Compiles but not yet functional
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@29 b9971538-fbc6-430a-b57f-16e266280561

--- oct_aplay.cc
+++ oct_aplay.cc
@@ -28,6 +28,7 @@
 #include <signal.h>
 
 #include <alsa/asoundlib.h>
+#include <poll.h>
 
 //
 // Octave headers.
@@ -63,6 +64,11 @@ using namespace std;
 #endif
 #define CLAMP(x, low, high)  (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
 
+#ifdef MIN
+#undef MIN
+#endif
+#define MIN(a, b) (a) < (b) ? (a) : (b)
+
 #define mxGetM(N)   args(N).matrix_value().rows()
 #define mxGetN(N)   args(N).matrix_value().cols()
 #define mxIsChar(N) args(N).is_string()
@@ -96,11 +102,23 @@ int xrun_recovery(snd_pcm_t *handle, int err);
 int write_loop(snd_pcm_t *handle,
 	       adata_type *samples,
 	       int channels);
-int set_hwparams(snd_pcm_t *handle);
+
+int set_hwparams(snd_pcm_t *handle,
+		 snd_pcm_format_t format,
+		 unsigned int *fs,
+		 unsigned int channels,
+		 snd_pcm_uframes_t *period_size,
+		 unsigned int *num_periods);
+
 int set_swparams(snd_pcm_t *handle, snd_pcm_uframes_t avail_min,
 		 snd_pcm_uframes_t start_threshold,
 		 snd_pcm_uframes_t stop_threshold);
 
+snd_pcm_sframes_t poll_loop(snd_pcm_t *handle,
+			    unsigned int nfds, 
+			    unsigned int poll_timeout,
+			    struct pollfd *pfd,
+			    snd_pcm_uframes_t period_size);
 
 
 /***
@@ -165,79 +183,86 @@ int write_loop(snd_pcm_t *handle,
 
 /***
  *
- * Setter hardware-parametre. Ved feil, print feilmelding og avslutt. 
+ * Setup the hardware parameters.
  *
  */
 
-int set_hwparams(snd_pcm_t *handle, driver_t *driver)
+int set_hwparams(snd_pcm_t *handle,
+		 snd_pcm_format_t format,
+		 unsigned int *fs,
+		 unsigned int channels,
+		 snd_pcm_uframes_t *period_size,
+		 unsigned int *num_periods)
 {
   snd_pcm_hw_params_t *hwparams;
+  snd_pcm_format_t tmp_format;
+  int direction, err;
+
   snd_pcm_hw_params_alloca(&hwparams);
   
-  int direction = 0;
-  int err;
-  
+  // Initialize the hw structure.
   if((err = snd_pcm_hw_params_any(handle, hwparams)) < 0){
-		fprintf(stderr, "Kan ikke initialisere parameter struktur: %s\n",
-			snd_strerror(err));
-		exit(-1);
+    fprintf(stderr, "Kan ikke initialisere parameter struktur: %s\n",
+	    snd_strerror(err));
+    //exit(-1);
   }
-
-  if((err = snd_pcm_hw_params_set_access(handle, hwparams,
-					 SND_PCM_ACCESS_MMAP_INTERLEAVED)) < 0){
+  
+  if((err = snd_pcm_hw_params_set_access(handle,hwparams,SND_PCM_ACCESS_MMAP_INTERLEAVED)) < 0){
     fprintf(stderr, "Kan ikke sette Aksesstype: %s\n",
 	    snd_strerror(err));
     //exit(-1);
   }
 
-  // Try to set format1, else set format 2, which should fit. 
-  if(snd_pcm_hw_params_test_format(handle, hwparams, 
-				   FORMAT1) == 0){
-    driver.format = FORMAT1;
-  }else{
-    driver.format = FORMAT2;
+  // Test if the audio hardwear supports the audio format otherwise use a fallback 
+  // audio format.
+  if(snd_pcm_hw_params_test_format(handle, hwparams,format) == 0){
+    tmp_format = format;
+  } else {
+    // Fallback format.
+    printf(" Warning: Cannot set the selected audio format. Falling back to SND_PCM_FORMAT_S16\n"); 
+    tmp_format = SND_PCM_FORMAT_S16;
   }
 
-  if((err = snd_pcm_hw_params_set_format(handle, hwparams,
-					 driver.format)) < 0){
+  // Set the audio format.
+  if((err = snd_pcm_hw_params_set_format(handle,hwparams,tmp_format)) < 0){
     fprintf(stderr, "Kan ikke sette sampleformat: %s\n",
 	    snd_strerror(err));
     //exit(-1);
   }
-  
-  if((err = snd_pcm_hw_params_set_rate_near(handle, hwparams,
-					    &(driver.rate), &direction)) < 0){
+
+  // Set the sampling frequency.
+  direction = 0;
+  if((err = snd_pcm_hw_params_set_rate_near(handle, hwparams,fs, &direction)) < 0){
     fprintf(stderr, "Kan ikke sette samplerate: %s\n",
 	    snd_strerror(err));
     //exit(-1);
   }
 
-  if((err = snd_pcm_hw_params_set_channels(handle, hwparams,
-					   driver.num_channels)) < 0){
+  // Set the number of channels.
+  if((err = snd_pcm_hw_params_set_channels(handle, hwparams,channels)) < 0){
     fprintf(stderr, "Kan ikke sette antall kanaler: %s\n",
 	    snd_strerror(err));
     //exit(-1);
   }
 
+  // Set approximate target period size in frames (Frames/Period). The chosen approximate target period 
+  // size is returned.
   direction = 0;
-  if((err = snd_pcm_hw_params_set_period_size_near(handle, hwparams,
-						   &(driver.period_size), &direction)) < 0){
-    fprintf(stderr, "Kan ikke sette periodestørrelse: %s\n",
-	    snd_strerror(err));
+  if((err = snd_pcm_hw_params_set_period_size_near(handle, hwparams,period_size, &direction)) < 0){
+    fprintf(stderr, "Kan ikke sette periodestørrelse: %s\n",snd_strerror(err));
     //exit(-1);
   }
 
+  // Set approximate number of periods in the buffer (Periods/Buffer). The chosen approximate number of
+  // periods per buffer is returned.
   direction = 0;
-  if((err = snd_pcm_hw_params_set_periods_near(handle, hwparams,
-					       &(driver.num_periods), &direction)) < 0){
-    fprintf(stderr, "Kan ikke sette antall perioder: %s\n",
-	    snd_strerror(err));
-    exit(-1);
+  if((err = snd_pcm_hw_params_set_periods_near(handle, hwparams,num_periods, &direction)) < 0){
+    fprintf(stderr, "Kan ikke sette antall perioder: %s\n",snd_strerror(err));
+    //exit(-1);
   }
 	
   if((err = snd_pcm_hw_params(handle, hwparams)) < 0){
-    fprintf(stderr, "Kan ikke sette HW parametre: %s\n",
-	    snd_strerror(err));
+    fprintf(stderr,"Kan ikke sette HW parametre: %s\n",snd_strerror(err));
     //exit(-1);
   }
   
@@ -251,7 +276,8 @@ int set_hwparams(snd_pcm_t *handle, driver_t *driver)
  *
  ***/
 
-int set_swparams(snd_pcm_t *handle, snd_pcm_uframes_t avail_min,
+int set_swparams(snd_pcm_t *handle,
+		 snd_pcm_uframes_t avail_min,
 		 snd_pcm_uframes_t start_threshold,
 		 snd_pcm_uframes_t stop_threshold)
 {
@@ -265,30 +291,36 @@ int set_swparams(snd_pcm_t *handle, snd_pcm_uframes_t avail_min,
     //exit(-1);
   }
  
-  if((err = snd_pcm_sw_params_set_avail_min(handle,
-					    swparams, avail_min)) < 0){
-    fprintf(stderr, "Kan ikke sette grense for minimum avail: %s\n",
+  // Set the minimum number of frames between interrupts. 
+  //
+  //Most PC sound cards can only accept power of 2 frame counts (i.e. 512, 1024, 2048).
+  if((err = snd_pcm_sw_params_set_avail_min(handle,swparams,avail_min)) < 0){
+    fprintf(stderr, "Cannot set minimum available count: %s\n",
 	    snd_strerror(err));
     //exit(-1);
   }
   
-  if((err = snd_pcm_sw_params_set_start_threshold(handle,
-						  swparams, start_threshold)) < 0){
-    fprintf(stderr, "Kan ikke sette start-terskel%s\n",
+  // Set the number of frames required before the PCM playback is started. 
+  //
+  // PCM is automatically started when playback frames available to PCM are >= start_threshold.
+  if((err = snd_pcm_sw_params_set_start_threshold(handle,swparams,start_threshold)) < 0){
+    fprintf(stderr, "Cannot set start mode (threshold) %s\n",
 	    snd_strerror(err));
     //exit(-1);
   }
 
-  if((err = snd_pcm_sw_params_set_stop_threshold(handle,
-						 swparams, stop_threshold)) < 0){
-    fprintf(stderr, "Kan ikke sette stop-terskel%s\n",
-	    snd_strerror(err));
+  // Set the pcm stop threshold.
+  //
+  // PCM is automatically stopped in SND_PCM_STATE_XRUN state when available frames is >= stop_threshold. 
+  // If the stop threshold is equal to boundary (also software parameter - sw_param) then automatic 
+  // stop will be disabled (thus device will do the endless loop in the ring buffer).
+  if((err = snd_pcm_sw_params_set_stop_threshold(handle,swparams,stop_threshold)) < 0){
+    fprintf(stderr, "Cannot set stop mode (threshold) : %s\n",snd_strerror(err));
     //exit(-1);
   }
   
   if((err = snd_pcm_sw_params(handle, swparams)) < 0){
-    fprintf(stderr, "Kan ikke sette SW parametre: %s\n",
-	    snd_strerror(err));
+    fprintf(stderr, "Cannot set the software parameters: %s\n",snd_strerror(err));
     //exit(-1);
   }
 
@@ -296,8 +328,111 @@ int set_swparams(snd_pcm_t *handle, snd_pcm_uframes_t avail_min,
 }
 
 
+/***
+ *
+ * Mainloop som holder styr på flere fildeskriptorer via en poll().
+ * Denne funksjonen er hentet fra prosjektet JACK (www.jackaudio.org),
+ * og tilpasset bruk her. 
+ *
+ * JACK is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU GPL and LGPL licenses as published by 
+ * the Free Software Foundation, <http://www.gnu.org> 
+ *
+ ***/
 
+snd_pcm_sframes_t poll_loop(snd_pcm_t *handle,
+			    unsigned int nfds, 
+			    unsigned int poll_timeout,
+			    struct pollfd *pfd,
+			    snd_pcm_uframes_t period_size)
+{
+  snd_pcm_sframes_t avail = 0;
+  snd_pcm_sframes_t play_avail = 0;
+  int need_play = 1;
+  unsigned int tmp_nfds; 
+  unsigned int i; // teller. 
+  unsigned int ci; // teller for capture fd. 
+  int xrun_true = 0;
 
+  // Poll-loop
+  while (need_play) { 
+    
+    int p_timeout; 
+    
+    // finne riktig antall poll descriptors. Dette kan variere med
+    // pcm-type
+    // ALSA fikser dette. Setter også riktige events. 
+    tmp_nfds = 0;
+    if (need_play){
+      snd_pcm_poll_descriptors(handle,&(pfd[tmp_nfds]),nfds);
+      tmp_nfds += nfds;
+    }
+    
+    // Legge til pollevent err. 
+    for (i = 0; i < tmp_nfds; i++)
+      pfd[i].events |= POLLERR;
+    
+    
+    if (poll (pfd, tmp_nfds,poll_timeout) < 0) {
+      // poll error. 
+      perror("poll error");
+      //return -1;
+    }
+		
+    p_timeout = 0;
+    if (need_play) {
+      // teller opp for playback.
+      for (i = 0; i < nfds; i++) {
+	if(pfd[i].revents & POLLERR){
+	  xrun_true = 1;
+	}
+				
+	if(pfd[i].revents == 0) {
+	  // Timeout. Ingen events. 
+	  p_timeout++;
+	}
+      }
+
+      if (p_timeout == 0){
+	// play har event. Trenger ikke mer poll.. 
+	need_play = 0;
+      }
+    }
+    
+    if (p_timeout && (p_timeout == nfds)) {
+      fprintf(stderr, "poll timeout.\n");
+      //return 0;
+    }
+
+  }
+    
+  
+  if ((play_avail = snd_pcm_avail_update(handle)) < 0) {
+    if(play_avail == -EPIPE){
+      xrun_true = 1;
+    } else {
+      fprintf(stderr, "Feil i avail_update(play)\n");
+      //return -1;
+    }
+  }
+  
+  if(xrun_true) {
+    printf("XRUN");
+    //xrun_recovery();
+    //return 0;
+  }
+  
+  avail = play_avail;
+  
+#ifdef DEBUG
+  fprintf(stderr, "poll loop, avail = %lu, playavail = %lu\n", MIN(avail, period_size), play_avail);
+#endif
+  
+  return MIN(avail, period_size);
+}
+  
+  
+  
 /***
  * 
  * Octave (oct) gateway function for APLAY.
@@ -320,19 +455,51 @@ Input parameters:\n\
   int err;
   //unsigned int i,m,n;
   octave_idx_type i,m,n;
-  int channels,fs;
   snd_pcm_t *handle;
-  snd_pcm_sframes_t frames,oframes;
-  driver_t driver; 
+  snd_pcm_sframes_t frames,frames_played;
+  unsigned int framesize;
+  unsigned int sample_bytes;
   adata_type *buffer;
   char device[50];
   int  buflen;
+  unsigned int nfds;
+  unsigned int poll_timeout;
+  struct pollfd *pfd;
+
   //char *device = "plughw:1,0";
   //char *device = "hw:1,0";
   //char *device = "default";
+
+  // HW parameters
+  snd_pcm_format_t format;
+  unsigned int fs;
+  unsigned int channels;
+  snd_pcm_uframes_t period_size;
+  unsigned int num_periods;
+
+  // SW parameters.
+  snd_pcm_uframes_t avail_min;
+  snd_pcm_uframes_t start_threshold;
+  snd_pcm_uframes_t stop_threshold;
+  
+  snd_pcm_uframes_t frames_to_write;
+  snd_pcm_sframes_t contiguous; 
+  snd_pcm_uframes_t nwritten;
+  snd_pcm_uframes_t offset; 
+  const snd_pcm_channel_area_t *play_areas;
+
   octave_value_list oct_retval; 
 
   int nrhs = args.length ();
+
+
+
+  // Set the ALSA audio data format.
+#ifdef USE_ALSA_FLOAT
+  format = SND_PCM_FORMAT_FLOAT;
+#else
+  format = SND_PCM_FORMAT_S16;
+#endif
 
   // Check for proper inputs arguments.
 
@@ -405,7 +572,7 @@ Input parameters:\n\
     device[buflen] = '\0';
     
   } else
-      strcpy(device,"default"); 
+    strcpy(device,"default"); 
 
 
   // Allocate buffer space.
@@ -422,62 +589,31 @@ Input parameters:\n\
     }
   }
 
-
-
-  /*
-  // Open in blocking mode (0, SND_PCM_NONBLOCK, or SND_PCM_ASYNC).
-  if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK,SND_PCM_NONBLOCK)) < 0) {
-  error("Playback open error: %s\n", snd_strerror(err));
-  return oct_retval;
-  }
-  
-  if ((err = snd_pcm_set_params(handle,
-  #ifdef USE_ALSA_FLOAT
-  SND_PCM_FORMAT_FLOAT, 
-  #else
-  SND_PCM_FORMAT_S16,
-  #endif
-  SND_PCM_ACCESS_RW_INTERLEAVED,
-  channels,
-  fs,
-  ALLOW_ALSA_RESAMPLE,
-  LATENCY)) < 0) {
-  error("Playback set params error: %s\n", snd_strerror(err));
-  snd_pcm_close(handle);
-  return oct_retval;
-  }
-  */
-
-
-  driver.device_name = device;
-  driver.block = 0; // (0, SND_PCM_NONBLOCK, or SND_PCM_ASYNC).
-  driver.rate = fs;
-  driver.period_size = 2;    // PERIOD_SIZE; ?
-  driver.num_periods = 64; //NUM_PERIODS; Number of frames between interupts (=latency?)
-  driver.num_channels = channels;
-  
-  // Åpne to PCM-devicer. En for lesing og en for skriving.
+  // Open the PCM playback device. 
   if ((err = snd_pcm_open(&handle,device,SND_PCM_STREAM_PLAYBACK,SND_PCM_NONBLOCK)) < 0) {
     error("Playback open error: %s\n", snd_strerror(err));
     return oct_retval;
   }
-  
-  set_hwparams(driver.play_handle);
-  // swparams: (handle, min_avail, start_thres, stop_thres)
-  set_swparams(handle,
-	       driver.period_size >> 1,
-	       driver.period_size >> 3,
-	       driver.period_size << 3);
-  
-  sample_bytes = snd_pcm_format_width(driver.format)/8;
-  framesize = channels * sample_bytes;
-  driver.buf = calloc(/*driver.num_periods **/ driver.period_size,
-		      driver.framesize);
 
+  // Setup the hardwear parameters for the playback device.
+  period_size = 2;
+  num_periods = 4096;
+  set_hwparams(handle,format,&fs,channels,&period_size,&num_periods);
+
+  // swparams: (handle, min_avail, start_thres, stop_thres)
+  avail_min = 4096; // Play 4096 frames before interrupt.
+  start_threshold = 0;
+  stop_threshold = 0;
+  set_swparams(handle,avail_min,start_threshold,stop_threshold);
+  
+  sample_bytes = snd_pcm_format_width(format)/8; // Compute the number of bytes per sample.
+  framesize = channels * sample_bytes; // Compute the framesize;
+  //driver.buf = calloc(period_size,framesize); // Should be frames*channels here.
+  
   nfds = snd_pcm_poll_descriptors_count(handle);
-  pfd = malloc(sizeof(struct pollfd));
-  poll_timeout = floor(1.5 * 1000000 * 
-			      driver.period_size / driver.rate);
+  pfd = (pollfd*)  malloc(sizeof(struct pollfd));
+  poll_timeout = (unsigned int) floor(1.5 * 1000000 * period_size / fs);
+
 #if 1
   // Infoutskrifter. 
   snd_output_t *snderr;
@@ -487,26 +623,73 @@ Input parameters:\n\
   snd_pcm_dump_setup(handle, snderr);
 #endif
 
-
-  //err = write_loop(handle,buffer,frames,channels);
-  //if (err < 0)
-  //  printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
-
-  /*  
-  oframes = snd_pcm_writei(handle, buffer, frames);
-
-  if (oframes < 0)
-    oframes = snd_pcm_recover(handle, oframes, 0);
+  //
+  // Write the audio data to the PCM device.
+  //
   
-  if (oframes < 0)
-    printf("snd_pcm_writei failed: %s\n", snd_strerror(err));
-  
-  if (oframes > 0 && oframes < frames)
-    printf("Short write (expected %li, wrote %li)\n", frames, oframes);
-  */
+  frames_played = 0;
+  while(frames - frames_played > 0) { // Loop until all frames are played.
+    
+    // Poll the playback device.
+    if ((frames_to_write = poll_loop(handle,nfds,poll_timeout,pfd,period_size)) < 0) {
+      fprintf(stderr, "Poll loop error\n");
+      //return NULL;
+    }
 
-  free(driver.pfd);
-  free(driver.buf);
+    if (frames_to_write >  (frames - frames_played) )
+      frames_to_write = frames - frames_played; 
+    
+    //printf("frames_to_write=%d\n",frames_to_write);
+    // Write  
+    if (frames_to_write > 0){
+      
+      nwritten = 0;
+      while(frames_to_write > 0){
+	
+	// ønsket sammenhengende område. 
+	contiguous = frames_to_write; 
+	
+	if ((err = snd_pcm_mmap_begin(handle,&play_areas,&offset,&frames_to_write)) < 0) {
+	  fprintf(stderr, "MMAP begin\n");
+	  //return -1;
+	}
+	//if (offset != 7)
+	//  printf("offset=%d\n",offset);
+	
+	// Copy audio data from the buffer to the MMAP:ed memory.
+	//memcpy( (((char*) play_areas->addr) + offset * framesize),
+	//	(((char*) buffer) + (frames_played+nwritten) * framesize),
+	//	(contiguous * framesize));
+	
+	if((err = snd_pcm_mmap_commit(handle,offset,contiguous)) < 0){
+	  fprintf(stderr, "MMAP commit error\n");
+	  //return -1;
+	}
+
+	printf("frames_to_write=%d offset=%d contiguous=%d\n",frames_to_write,offset,contiguous);
+	printf("Remainig frames = %d frames_played = %d\n",frames - frames_played,frames_played );
+	if (contiguous > 0) {
+	  frames_to_write -= contiguous;
+	  nwritten += contiguous;
+	}
+
+      }
+    }
+    frames_played += nwritten;
+    
+    
+    if (snd_pcm_state(handle) < 3) {
+      snd_pcm_start(handle);
+    }
+
+  }
+
+
+  //
+  // Cleanup.
+  //
+
+  free(pfd);
   snd_pcm_close(handle);
   free(buffer);
   
