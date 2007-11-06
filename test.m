b9c52abb005ce219db0dd1d9c7153be63e8a3a51
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2007-11-06 15:30:30 +0000

    Added repmat to get more channels.
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@40 b9971538-fbc6-430a-b57f-16e266280561

--- test.m
+++ test.m
@@ -1,10 +1,16 @@
-function test(channels)
+function test(channels,device)
 
 if nargin < 1
   channels = 2;
 end
 
+if nargin < 2
+  device = 'hw:0,0';
+end
+
+
+
 x = randn(10*44100,1);
 u = sin((1:128*1024)/(16*1024)*400*pi);
 
-aplay(repmat(u(:),1,10),48000,'hw:0,0');
+aplay(repmat(u(:),1,channels),48000,device);
