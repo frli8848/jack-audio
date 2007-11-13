f302d3769f97cbed924f59faf29551c041a15f20
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2007-11-13 15:24:52 +0000

    Added aaudio and ainfo.
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@87 b9971538-fbc6-430a-b57f-16e266280561

--- build_aaudio.m
+++ build_aaudio.m
@@ -4,6 +4,8 @@
 
 
 % Octave
-mkoctfile -v oct_aplay.cc   -lasound -o aplay.oct
-mkoctfile -v oct_arecord.cc -lasound -o arecord.oct
-mkoctfile -v oct_aplayrec.cc -lasound -o arecord.oct
+mkoctfile -v aaudio.cc -c 
+mkoctfile -v oct_aplay.cc    aaudio.o -lasound -o aplay.oct
+mkoctfile -v oct_arecord.cc  aaudio.o -lasound -o arecord.oct
+mkoctfile -v oct_aplayrec.cc aaudio.o -lasound -lpthread -o arecord.oct
+mkoctfile -v oct_ainfo.cc    aaudio.o -lasound  -o ainfo.oct
