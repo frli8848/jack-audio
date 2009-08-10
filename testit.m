c5214e52c24d3234ecc49abad9ad3e9523f52072
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2009-08-10 11:00:57 +0000

    Added a test file for the atrecord function.
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@153 b9971538-fbc6-430a-b57f-16e266280561

---
+++ testit.m
@@ -0,0 +1,25 @@
+
+
+Fs = 44100;
+L = 10*44100;
+
+channels = 2;
+
+trigger_level = 0.01;
+trigger_channel = 0;
+trigger_frames = 0.1*Fs;
+trigger_par = [trigger_level trigger_channel trigger_frames];
+
+y = atrecord(trigger_par,L,channels,Fs,'hw:0,0');
+
+subplot(211)
+plot((0:L-1)/Fs,y(:,1));
+grid on
+ax = axis;
+line([5 5],[ax(3) ax(4)]);
+
+subplot(212)
+plot((0:L-1)/Fs,y(:,2));
+grid on
+ax = axis;
+line([5 5],[ax(3) ax(4)]);
