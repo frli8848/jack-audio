99e80aff59379f6a1e1bc9b29b700ec9b767a48a
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2011-09-01 13:39:13 +0000

    Switched to using octave_idx_type for all interger input pars (since they are used to reference memory).
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@227 b9971538-fbc6-430a-b57f-16e266280561

--- jaudio.h
+++ jaudio.h
@@ -32,17 +32,17 @@ void set_running_flag(void);
 void clear_running_flag(void);
 
 int play_finished(void);
-int play_init(void* buffer, octave_idx_type frames, int channels, char **port_names);
+int play_init(void* buffer, octave_idx_type frames, octave_idx_type channels, char **port_names);
 int play_close(void);
 
 int record_finished(void);
-int record_init(void* buffer, octave_idx_type frames, int channels, char **port_names);
+int record_init(void* buffer, octave_idx_type frames, octave_idx_type channels, char **port_names);
 int record_close(void);
 
 
 int t_record_finished(void);
 int t_record_process(jack_nframes_t nframes, void *arg);
-int t_record_init(void* buffer, octave_idx_type frames, int channels, char **port_names,
+int t_record_init(void* buffer, octave_idx_type frames, octave_idx_type channels, char **port_names,
 		  double trigger_level,
 		  octave_idx_type trigger_channel,
 		  octave_idx_type trigger_frames,
