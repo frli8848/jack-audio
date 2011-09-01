a5e32ec11b11b91c569b0e232546612a50167063
fl <fl@b9971538-fbc6-430a-b57f-16e266280561>
2011-09-01 07:39:09 +0000

    Updated the octave pkg build script to build both the ALSA and the JACK packages and made new Makefiles for the respectively package.
    git-svn-id: svn+ssh://ludde.lingvall.no/mnt/subversion/repos/aaudio/trunk@224 b9971538-fbc6-430a-b57f-16e266280561

--- build_pkg.sh
+++ build_pkg.sh
@@ -2,13 +2,18 @@
 #
 # Scrip to build the aaudio octave package.
 #
+# Copyright (C) 2008, 2009, 2011 Fredrik Lingvall.
 #
-#
-# Copyright (C) 2008, 2009 Fredrik Lingvall.
+# 
 
-VERSION=0.2.1
+
+VERSION=0.2.0
 
 echo "Note add version number $VERSION to the DESCRIPTION file."
+
+#
+# ALSA
+#
 
 rm -fR aaudio-$VERSION
 mkdir -p aaudio-$VERSION
@@ -17,11 +22,9 @@ mkdir -p aaudio-$VERSION/src
 mkdir -p aaudio-$VERSION/doc
 mkdir -p aaudio-$VERSION/
 
-#
-# The sources
-#
+# The ALSA sources
 
-cp Makefile aaudio-$VERSION/src/
+cp Makefile_octave aaudio-$VERSION/src/Makefile
 cp aaudio.h aaudio-$VERSION/src/
 cp aaudio.cc aaudio-$VERSION/src/
 cp oct_aplay.cc aaudio-$VERSION/src/
@@ -30,11 +33,39 @@ cp oct_atrecord.cc aaudio-$VERSION/src/
 cp oct_aplayrec.cc aaudio-$VERSION/src/
 cp oct_ainfo.cc aaudio-$VERSION/src/
 
-#
 # Mandatory pkg files
-#
 
 cp COPYING aaudio-$VERSION/
 cp DESCRIPTION aaudio-$VERSION/ 
 
 tar cvzf aaudio-$VERSION.tar.gz aaudio-$VERSION/ 
+
+
+#
+# ALSA
+#
+
+rm -fR jaudio-$VERSION
+mkdir -p jaudio-$VERSION
+mkdir -p jaudio-$VERSION/inst
+mkdir -p jaudio-$VERSION/src
+mkdir -p jaudio-$VERSION/doc
+mkdir -p jaudio-$VERSION/
+
+# The JACK sources
+
+cp Makefile_jack jaudio-$VERSION/src/Makefile
+cp jaudio.h jaudio-$VERSION/src/
+cp jaudio.cc jaudio-$VERSION/src/
+cp oct_jplay.cc jaudio-$VERSION/src/
+cp oct_jrecord.cc jaudio-$VERSION/src/
+cp oct_jtrecord.cc jaudio-$VERSION/src/
+cp oct_jplayrec.cc jaudio-$VERSION/src/
+cp oct_jinfo.cc jaudio-$VERSION/src/
+
+# Mandatory pkg files
+
+cp COPYING jaudio-$VERSION/
+cp DESCRIPTION jaudio-$VERSION/ 
+
+tar cvzf jaudio-$VERSION.tar.gz jaudio-$VERSION/ 
